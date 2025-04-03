800ba5f12775b9ef3bedb7c8ca4cfd7fa5313701
andkoehlert <Andkoehlert@outlook.com>
2025-04-03 15:01:07 +0200

    test nuxt auth. Didn't use it

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import cors from 'cors';
+
 import express, {Application, Request, Response} from 'express'
 import DotenvFlow from 'dotenv-flow';
 import routes from './routes';
@@ -10,13 +12,15 @@ DotenvFlow.config();
 // create express application
 const app: Application = express();
 
-
-
-
-
 export function startServer() {
   
-
+  app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    methods: 'GET, PUT, POST, DELETE',
+    allowedHeaders: ['auth-token', 'Origin', 'X-Requested-With', 'Content-Type', 'Accepts'],
+  }));
+  
   // JSON body parser - so it can understand json.
   app.use(express.json());
 
